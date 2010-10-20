1718fef730e10afa89fe96b2d7e8acfb83f32231
Evgeny Nesterov <en.varg@gmail.com>
2010-10-20 16:59:24 +0400

    deploy db

--- config/config.php
+++ config/config.php
@@ -15,6 +15,7 @@
 		$cfg->set_connections(array(
 			'test' => 'mysql://root:123456@localhost/test',
 			'develop' => 'mysql://baseadmin:C0ffeAmerikan0@192.168.1.226:33306/mvideo',
+			'deploy' => 'mysql://baseadmin:C0ffeAmerikan0@diablopeerslave:33306/mvideo',
 			'develop_test' => 'mysql://baseadmin:C0ffeAmerikan0@localhost:33306/mvideo',
 		));
 	});
