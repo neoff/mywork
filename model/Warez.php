32de0bf0c4042ef2b33ac3f886596fccf3366583
Evgeny Nesterov <en.varg@gmail.com>
2010-12-09 15:50:42 +0300

    category

--- model/Warez.php
+++ model/Warez.php
@@ -80,8 +80,8 @@ class Warez extends ActiveRecord\Model
 			
 		$sql = 'select * from `warez_' .$region_id . '` 
 				where ' . $sql_impl. $limit;
-		print $sql_impl;
-		print $sql;
+		//print $sql_impl;
+		//print $sql;
 		return self::find_by_sql($sql);
 	}
 	
