8141853757784a02830d39000fc2d0e7e85a9260
Evgeny Nesterov <en.varg@gmail.com>
2010-12-09 17:45:53 +0300

    category

--- controllers/Category.php
+++ controllers/Category.php
@@ -289,8 +289,8 @@ class ControllerCategory extends Template\Template{
 			$this->parent_id = $this->category_id;
 		}
 		$this->parent_category="";
-		$this->parent_category->addChild("category_id", $cat_parrent_id);
-		$this->parent_category->addChild("category_name", $cat_parrent_name);
+		//$this->parent_category->addChild("category_id", $cat_parrent_id);
+		//$this->parent_category->addChild("category_name", $cat_parrent_name);
 		
 		return $this->options;
 	}
