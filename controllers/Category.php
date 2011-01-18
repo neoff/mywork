342045763ce6ff68f382aceb5660750410506467
Evgeny Nesterov <en.varg@gmail.com>
2011-01-18 16:59:35 +0300

    unit test

--- controllers/Category.php
+++ controllers/Category.php
@@ -170,10 +170,10 @@ class ControllerCategory extends Template\Template{
 		foreach ($this->category as $key => $val)
 		{
 			$amount = $this->recurseAmount($val);
-			print "\n\n\n ================================================" . $val->name. " ". $amount."\n\n\n";
+			#print "\n\n\n ================================================" . $val->name. " ". $amount."\n\n\n";
 			if($amount == 0 )
 				continue;
-			print "\n\n\n ================================================" . $val->name. " ". $amount."\n\n\n";
+			#print "\n\n\n ================================================" . $val->name. " ". $amount."\n\n\n";
 			$category = $this->categories->addChild("category");
 			$category->addChild("category_id", $val->category_id);
 			$category->addChild("category_name", ToUTF($val->name));
