4ec0089fb3d9a48ee9db5c88bf62c04923ba515b
Evgeny Nesterov <en.varg@gmail.com>
2011-01-18 17:35:57 +0300

    unit test

--- controllers/Category.php
+++ controllers/Category.php
@@ -595,7 +595,7 @@ class ControllerCategory extends Template\Template{
 			{
 				$val = Models\Category::find('first',array('parent_id' => $key));
 				print_r($val);
-				$amount = $val->amount($val);
+				$amount = $amount = Models\Category::count(array('conditions' => "parent_id = $val->category_id"));
 				$key = $val->category_id;
 				$value['name'] = $val->name;
 			}
