f8513ef2ec68bf11e7fdcb683914c858f67b05a5
Evgeny Nesterov <en.varg@gmail.com>
2011-01-18 18:26:46 +0300

    unit test

--- example/show.py
+++ example/show.py
@@ -52,7 +52,11 @@ class xmlParser():
 	def getContent(self, dat):
 		d = xml.dom.minidom.parseString(dat)
 		elems = d.getElementsByTagName("parent_category")
-		categories = d.getElementsByTagName("categories")[0]
+		categories = d.getElementsByTagName("categories")
+		if categories:
+			categories = d.getElementsByTagName("products")[0]
+		else:
+			categories = categories[0]
 		for elem in elems:
 			
 			parent = elem.getElementsByTagName("category_id")[0]
