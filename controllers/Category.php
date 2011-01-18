633f28f8254b17586f4bc0887aa0e286a57c1116
Evgeny Nesterov <en.varg@gmail.com>
2011-01-18 17:59:16 +0300

    unit test

--- controllers/Category.php
+++ controllers/Category.php
@@ -364,7 +364,7 @@ class ControllerCategory extends Template\Template{
 					if(in_array($this->parents->dirid, self::$GlobalConfig['smenu'][$key]['dirs']))
 					{
 						$cat_parrent_id = ToUTF($key);
-						print_r(array_count_values($value['dirs']));
+						print_r(count($value['dirs']));
 						print $cat_parrent_id;
 						$cat_parrent_name = ToUTF(self::$GlobalConfig['smenu'][$key]['name']);
 						break;
