8d095514e0987d5e718961b7fa1709b2549ed2e3
Evgeny Nesterov <en.varg@gmail.com>
2011-01-27 17:57:53 +0300

    city pregreplace

--- controllers/Category.php
+++ controllers/Category.php
@@ -401,6 +401,8 @@ class ControllerCategory extends Template\Template{
 					$p = Models\Category::first(array('category_id' => $cat_parrent_id));
 					$cat_parrent_name = ToUTF($p->name);
 				}
+			else 
+				$this->parents->name = $cat_parrent_name;
 			$this->options = array('parent_id' => $this->category_id);
 			$this->parent_name = ToUTF($this->parents->name);
 			$this->parent_id = $this->category_id;
