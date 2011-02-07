d7f9c40ba9f9dca7a2ef52137cab0a4851c8557c
Evgeny Nesterov <en.varg@gmail.com>
2011-02-07 17:59:24 +0300

    images

--- controllers/Category.php
+++ controllers/Category.php
@@ -247,7 +247,7 @@ class ControllerCategory extends Template\Template{
 		
 		$this->categories="";
 		$this->categories->addAttribute("category_id", $this->category_id);
-		$this->categories->addAttribute("category_name", self::$GlobalConfig['smenu'][$this->category_id]['name']);
+		$this->categories->addAttribute("category_name", $this->parent_name);
 		
 		
 		$q = 'SELECT distinct w.DirID as result 
