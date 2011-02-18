4057a143f359213aa5a9ccca820400908e2c1c8d
Evgeny Nesterov <en.varg@gmail.com>
2011-02-18 18:46:56 +0300

    reparsing

--- controllers/CategoryImage.php
+++ controllers/CategoryImage.php
@@ -25,7 +25,7 @@
 	define( "DEBUG", ($conn->debug)?$conn->debug:True );
 	define( 'CONNECTION', ($conn->base)?$conn->base:'develop' );
 	$_SERVER['HTTP_HOST'] = "localhost";
-	$_SERVER["SCRIPT_FILENAME"] = dirname("~/newmvideo/www/");
+	$_SERVER["SCRIPT_FILENAME"] = "/www/sites/newmvideo/www/mobile/controllers";
 	
 	require_once ROOT_PATH . '/lib/ActiveRecord.php';
 	require_once ROOT_PATH . '/lib/Template.php';
