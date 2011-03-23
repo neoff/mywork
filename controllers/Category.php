0e4d06118d9b905afe984a7ec1c27d7e9679dc89
Evgeny Nesterov <en.varg@gmail.com>
2011-03-23 13:04:29 +0300

    param

--- controllers/Category.php
+++ controllers/Category.php
@@ -639,8 +639,8 @@ class ControllerCategory extends Template\Template{
 				$this->pages->addChild("onpage", "20");
 				$this->pages->addChild("page", $this->page);
 			}
-			var_dump($productes_m);
-			exit();
+			//var_dump($productes_m);
+			//exit();
 			if($productes_m)
 				foreach ($productes_m as $key => $val)
 				{
